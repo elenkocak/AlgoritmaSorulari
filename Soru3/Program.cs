bc6141881a1bc743f38ad3bcfd7650fe225b8859
Elen Ko�ak <elen.kocak@coino.com>
2022-12-01 00:51:46 +0300

    dg

--- Soru3/Program.cs
+++ Soru3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Net;
 
 namespace Soru3
 {
@@ -9,25 +10,39 @@ namespace Soru3
     {
         static void Main(string[] args)
         {
-            int[] dizi;
-            Console.WriteLine("Gireceğiniz puan adedini yazınız");
+            
+            Console.WriteLine("ADET GİRİNİZ");
             int adet=int.Parse(Console.ReadLine());
-            if (adet>0)
+            for (int i = 1; i <= adet; i++)
             {
-                dizi=new int[adet];
-                for (int i=0; i<dizi.Length; i++)
+                Console.WriteLine("Puanlarını gir");
+                List<int> list = new List<int>(int.Parse(Console.ReadLine()));
+                for (int j = 1; j <= adet; i++)
                 {
-                    int eklenenPuan;
-                    Console.WriteLine("Puanlarınızı giriniz");
-                   
-                    eklenenPuan=int.Parse(Console.ReadLine());
-                    dizi[i]=eklenenPuan;
-
-                   
+                    Console.WriteLine(list[i]);
                 }
+               
             }
+           
+            //int[] dizi;
+            //Console.WriteLine("Gireceğiniz puan adedini yazınız");
+            //int adet=int.Parse(Console.ReadLine());
+            //if (adet>0)
+            //{
+            //    dizi=new int[adet];
+            //    for (int i=0; i<dizi.Length; i++)
+            //    {
+            //        int eklenenPuan;
+            //        Console.WriteLine("Puanlarınızı giriniz");
+
+            //        eklenenPuan=int.Parse(Console.ReadLine());
+            //        dizi[i]=eklenenPuan;
+
+
+            //    }
+        }
            
             
         }
     }
-}
+
